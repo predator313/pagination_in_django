4471834767f4e9ee25ff30f4d714c996f0940964
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-25 19:03:15 +0530

    rendering dummy data to home page

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def post_list(request):
+    all_post=Post.objects.all()
+    return render(request,'myapp/home.html',{'posts':all_post})
