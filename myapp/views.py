9effcf6aea15790519e8c6e233095f5398f7f66c
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-25 19:14:53 +0530

    paginator is working fine

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Post
 # Create your views here.
 def post_list(request):
-    all_post=Post.objects.all()
-    return render(request,'myapp/home.html',{'posts':all_post})
+    all_post=Post.objects.all().order_by('pk')
+    paginator=Paginator(all_post,3)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+    return render(request,'myapp/home.html',{'posts':page_obj})
