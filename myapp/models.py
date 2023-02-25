0bb5ce0a3155800e658df055a4179b9977b31e28
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-25 18:38:30 +0530

    make migrations and migrate

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+#pagination is way to display the content in the multiple page
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    desc=models.TextField(max_length=500)
+    publish_date=models.DateTimeField()
